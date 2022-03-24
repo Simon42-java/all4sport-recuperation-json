a171c3c8ff9347bf1851187852497db3051ac0ee
Simon42-java <simon.basserue@ltpdampierre.fr>
2022-03-24 16:17:21 +0100

    au scan affichage du cout de la ref et du nom

--- app/src/main/java/com/example/all4sport/PageProduit.java
+++ app/src/main/java/com/example/all4sport/PageProduit.java
@@ -7,6 +7,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class PageProduit extends AppCompatActivity {
@@ -27,11 +31,41 @@ public class PageProduit extends AppCompatActivity {
             token = APIConnection.get(url);
             Toast toast = Toast.makeText(PageProduit.this, token, Toast.LENGTH_LONG);
             toast.show();
+            JSONObject jObj = null;
+
+            JSONArray mJsonArray = new JSONArray(token);
+            JSONObject mJsonObject = mJsonArray.getJSONObject(0);
+
+            String ref = mJsonObject.getString("pr_reference");
+            String cout = mJsonObject.getString("pr_cout_unitaire_HT");
+            String nom = mJsonObject.getString("pr_description");
+
+
+                            //JSONObject json = new JSONObject("{\"pr_reference\":1,\"pr_cout_unitaire_HT\":1,\"pr_description\":\"aaaa\",\"fk_ba\":1,\"fk_mo\":1,\"fk_ra\":\"1\",\"fk_se\":1,\"fk_et\":1,\"fk_fournisseur\":1,\"fk_rayon\":1\"}");
+                            //JSONArray json = new JSONArray(token);
+                            //{"pr_description": "aaa"}
+                            //j'ai ça
+                            //{"pr_reference":1,"pr_cout_unitaire_HT":1,"pr_description":"aaaa","fk_ba":1,"fk_mo":1,"fk_ra":"1","fk_se":1,"fk_et":1,"fk_fournisseur":1,"fk_rayon":1}
+                            //je veut recuperer ça
+                            //"{\"pr_reference\":1,\"pr_cout_unitaire_HT\":1,\"pr_description\":\"aaaa\",\"fk_ba\":1,\"fk_mo\":1,\"fk_ra\":\"1\",\"fk_se\":1,\"fk_et\":1,\"fk_fournisseur\":1,\"fk_rayon\":1\"}"
+                            //String test = json.getString("fk_ba");
+
+
             setContentView(R.layout.activity_page_produit);
-            TextView tv = (TextView) findViewById(R.id.textproduit);
-            tv.setText(token);
+            TextView tv_ref = (TextView) findViewById(R.id.tv_ref);
+            tv_ref.setText(ref);
+
+            TextView tv_nom = (TextView) findViewById(R.id.tv_nom);
+            tv_nom.setText(nom);
+
+            TextView tv_cout = (TextView) findViewById(R.id.tv_cout);
+            tv_cout.setText(cout);
+
+
+
+
             Log.e("aaaaaaa", token);
-        } catch (IOException e) {
+        } catch (IOException | JSONException e) {
             Log.e("OEEEEEEEEEE C'EST LA D", e.toString());
         }
 
